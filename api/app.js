ffb412c12cc4bb371a9045a42478ada0f6cab574
daiki510 <ddgg7755@gmail.com>
2021-03-21 23:28:06 +0900

    appの修正

--- api/app.js
+++ api/app.js
@@ -4,7 +4,6 @@ const port = process.env.PORT || 3001
 const mongoose = require("mongoose")
 const Comic = require("./models/comicModel")
 const routes = require("./routes/comicRoutes"); // Routeのインポート
-const bodyParser = require("body-parser");
 const connectOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -14,8 +13,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/Comicdb", connectOptions);
 
 //TODO:bodyParserが正常に動作しないためにjsonを読み取れない
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 routes(app); //appにRouteを設定する。
 
